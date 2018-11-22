d73204053fea9e41f87211549f3a6495cee9c1cb
tylerbryto <tyler.brito99@gmail.com>
2018-11-21 22:07:49 -0200

    isolating swagger config

---
+++ src/Trinity/Trinity.Infra.CrossCutting.Swagger/SwaggerConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Trinity.Infra.CrossCutting.Swagger
+{
+    public class SwaggerConfig
+    {
+        public static void UseSwagger(IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Trinity API V1");
+                c.RoutePrefix = string.Empty;
+            });
+        }
+
+        public static void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Trinity API",
+                    Description = "ASP.NET Core Web API for Bands, Albums and Musics management",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Tyler Mendes de Brito",
+                        Email = "tyler.brito99@gmail.com",
+                        Url = "https://github.com/tylerbryto"
+                    },
+                });
+
+                string xmlFile = "Trinity.Presentation.WebApi.xml";//$"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+    }
+}
--- src/Trinity/Trinity.Presentation.WebApi/Startup.cs
+++ src/Trinity/Trinity.Presentation.WebApi/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
 using Trinity.Infra.CrossCutting.IoC;
+using Trinity.Infra.CrossCutting.Swagger;
 
 namespace Trinity.Presentation.WebApi
 {
@@ -23,9 +24,11 @@ namespace Trinity.Presentation.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             DependencyInjector.RegisterServices(services);
-            AddSwagger(services);
+            SwaggerConfig.AddSwagger(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,43 +43,11 @@ namespace Trinity.Presentation.WebApi
                 app.UseHsts();
             }
 
-            UseSwagger(app);
+            SwaggerConfig.UseSwagger(app);
             app.UseHttpsRedirection();
             app.UseMvc();
         }
 
-        void UseSwagger(IApplicationBuilder app)
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Trinity API V1");
-                c.RoutePrefix = string.Empty;
-            });
-        }
-
-        void AddSwagger(IServiceCollection services)
-        {
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Info
-                {
-                    Version = "v1",
-                    Title = "Trinity API",
-                    Description = "ASP.NET Core Web API for Bands, Albums and Musics management",
-                    TermsOfService = "None",
-                    Contact = new Contact
-                    {
-                        Name = "Tyler Mendes de Brito",
-                        Email = "tyler.brito99@gmail.com",
-                        Url = "https://github.com/tylerbryto"
-                    },
-                });
-
-                string xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
-            });
-        }
+        
     }
 }
